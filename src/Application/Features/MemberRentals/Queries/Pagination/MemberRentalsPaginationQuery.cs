5a6ad570197f4f377773aac7e2aa1e8f7f256849
hualin.zhu <neo.js.cn@gmail.com>
2025-10-25 11:11:09 +0800

    Enhance query logging and simplify UI fields
    Updated `ToString` methods in `MemberRentalsWithPaginationQuery` and `MembersWithPaginationQuery` to include `CardId` and `LicensePlate` for improved logging and debugging. Simplified UI in `MemberRentals.razor` and `Members.razor` by removing `Adornment` and `AdornmentIcon` from search fields.

--- src/Application/Features/MemberRentals/Queries/Pagination/MemberRentalsPaginationQuery.cs
+++ src/Application/Features/MemberRentals/Queries/Pagination/MemberRentalsPaginationQuery.cs
@@ -25,7 +25,7 @@ public class MemberRentalsWithPaginationQuery : MemberRentalAdvancedFilter, ICac
 {
     public override string ToString()
     {
-        return $"Listview:{ListView}:{CurrentUser?.UserId}, Search:{Keyword}, {OrderBy}, {SortDirection}, {PageNumber}, {PageSize}";
+        return $"Listview:{ListView}:{CurrentUser?.UserId}, Search:{Keyword}, {OrderBy}, {SortDirection}, {PageNumber}, {PageSize},{CardId},{LicensePlate}";
     }
     public string CacheKey => MemberRentalCacheKey.GetPaginationCacheKey($"{this}");
     public IEnumerable<string>? Tags => MemberRentalCacheKey.Tags;
